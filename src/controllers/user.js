be37bed7c82afcc604d6e72109326473d13689bb
IagorSs <iagor.penaranda@uol.com.br>
2022-11-11 14:52:54 -0300

    feat(user): cpf and email format validations

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,20 @@
 import UserModel from '../models/User.js';
 
+const isEmailFormatted = (email) => {
+  return email
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    ) !== null;
+}
+
+const isCPFFormatted = (cpf) => {
+  return cpf
+    .match(
+      /[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}/
+    ) !== null;
+}
+
 const isUserModelValid = (userModel) => {
   return typeof(userModel.cpf) === 'string'
     && typeof(userModel.nome) === 'string'
@@ -18,6 +33,18 @@ export const register = async (req, res) => {
     .status(400)
     .json({
       message: "Invalid request body"
+    });
+
+  if(!isEmailFormatted(userData.email)) return res
+    .status(400)
+    .json({
+      message: "Unformatted email"
+    });
+
+  if(!isCPFFormatted(userData.cpf)) return res
+    .status(400)
+    .json({
+      message: "Unformatted cpf"
     });
 
   if (await getUserByEmail(userData.email)) return res
