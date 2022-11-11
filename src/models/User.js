e7731fb471ead5a44c0aa1979beca937f93d890c
IagorSs <iagor.penaranda@uol.com.br>
2022-11-11 13:29:27 -0300

    feat(user): implement user model

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../config/database';
+
+class User extends Model {}
+
+User.init({
+  cpf: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  nascimento: DataTypes.DATE,
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  sequelize
+});
+
+export default User;
